55a943387f2cd5aa381d9142a74e733d03fc16a1
Salohiddin <00009998id@gmail.com>
2023-02-13 11:36:29 +0500

    smth

--- url_save/settings.py
+++ url_save/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY", default="SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["161.35.73.159", "*"]
 
 
 # Application definition
